5bf8527f611ac5bab2c88721bf4befac63260923
fab48 <fab7696550@gmail.com>
2016-12-06 10:58:42 -0500

    add test for show cites

--- spec/features/cites/show_list_cites_spec.rb
+++ spec/features/cites/show_list_cites_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 feature 'An user want a show list cites' do
-  context "and is logged " do
+  context "and is logged" do
+    before do
+      #create functionary
+      @functionary = FactoryGirl.create(:functionary)
+
+      # create patient
+      @patient = FactoryGirl.create(:patient)
+
+      # create doctor
+      @doctor = FactoryGirl.create(:doctor)
+    end
+    it "as patient" do
+      params = { email: @patient.email, password: "12345678" }
+      post "/api/v1/auth/sign_in", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+      expect(response.status).to eq(200)
+      rta = response
+
+      post "/api/v1/cites", params.to_json, { 'CONTENT_TYPE': 'application/json', 'ACCEPT': 'application/json', 'access-token': rta['access-token'], 'expiry': rta['expiry'], 'token-type': rta['token-type'], 'uid': rta['uid'], 'client': rta['client'] }
+
+    end
+
+    it "as doctor" do
+    end
+
+    it "as functionary" do
+    end
+  end
+
+  context "and is not logged" do
     before do
       #create functionary
       @functionary = FactoryGirl.create(:functionary)
